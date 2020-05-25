b7229541cbf8a1c7d1e7d4a5fb933de84c1d0bea
Frank Rauen <home@Franks-MacBook-Air.local>
2020-05-25 14:49:16 -0600

    Finishes Calculator project

--- src/App.js
+++ src/App.js
@@ -43,75 +43,106 @@ class App extends Component {
     }
   }
 
-  opInput(e) {
-    if(this.state.equation.includes('=')) {
+  operInput(e){
+    if(this.state.equation.includes("=")){
       let val = this.state.display;
       val += e.currentTarget.value;
       this.setState({
         equation: val
       });
     } else {
-      if(this.state.equation != '' && this.state.equation.match(/[*\-\/+]$/) == null) {
+      if(this.state.equation != "" && this.state.equation.match(/[*\-\/+]$/) == null){
         let val = this.state.equation;
         val += e.currentTarget.value;
         this.setState({
           equation: val
         });
-      } else if(this.state.equation.match(/[*\-\/+]$/) != null) {
+      } else if(this.state.equation.match(/[*\-\/+]$/) != null){
         let val = this.state.equation;
+        val = val.substring(0, (val.length-1));
         val += e.currentTarget.value;
         this.setState({
           equation: val
         });
-      } 
+      }
     }
-}
+  }
 
-decInput(e) {
-  if(this.state.equation == '' || this.state.equation.includes('=')){
-    let val = '0.';
+  decInput(e){
+    if(this.state.equation == "" || this.state.equation.includes("=")){
+      let val = '0.';
+      this.setState({
+        display: val,
+        equation: val
+      });
+    } else if(this.state.equation.match(/[+\-*\/]$/)){
+      let val = '0.';
+      this.setState({
+        display: val,
+        equation: this.state.equation + val
+      });
+    } else if(!this.state.display.includes(".")){
+      this.setState({
+        display: this.state.display + e.currentTarget.value,
+        equation: this.state.equation + e.currentTarget.value
+      });
+    }
+  }
+
+  clearInput(){
     this.setState({
-      display: val,
-      equation: val
+      display: "0",
+      equation: ""
     });
-  } else if(this.state.equation.match(/[+\-*\/]$/)) {
-    let val = '0.';
-    this.setState({
-      display: val,
-      equation: this.state.equation + val
-    });
-  } else if(!this.state.display.includes('.')) {
-    this.setState({
-      display: this.state.display + e.currentTarget.value,
-      equation: this.state.equation + e.currentTarget.value
-    });
+  }
+
+  calculate(){
+    if(this.state.equation.includes("=")){
+      let val = `${this.state.display} = ${this.state.display}`;
+      this.setState({
+        equation: val
+      });
+    } else if(this.state.equation != "" && this.state.equation.match(/[+\-*\/]/) != null && this.state.equation.match(/[+\-*\/]$/) == null) {
+      let result = Number.isInteger(eval(this.state.equation)) ? eval(this.state.equation) : parseFloat(eval(this.state.equation).toFixed(5));
+      let val = this.state.equation;
+      val += ` = ${result}`;
+      this.setState({
+        display: result,
+        equation: val
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Display equation={this.state.equation} display={this.state.display} />
+        <Button id="clear" value="clear" display="AC" class="row-3 col-1" click={this.clearInput} />
+        <Button id="sign" value="+/-" display="±" class="row-3 col-2" />
+        <Button id="percent" value="%" display="%" class="row-3 col-3" />
+        <Button id="divide" value="/" display="÷" class="oper row-3 col-4" click={this.operInput} />
+        <Button id="seven" value="7" display="7" class="num row-4 col-1" click={this.numInput} />
+        <Button id="eight" value="8" display="8" class="num row-4 col-2" click={this.numInput} />
+        <Button id="nine" value="9" display="9" class="num row-4 col-3" click={this.numInput} />
+        <Button id="multiply" value="*" display="×" class="oper row-4 col-4" click={this.operInput} />
+        <Button id="four" value="4" display="4" class="num row-5 col-1" click={this.numInput} />
+        <Button id="five" value="5" display="5" class="num row-5 col-2" click={this.numInput} />
+        <Button id="six" value="6" display="6" class="num row-5 col-3" click={this.numInput} />
+        <Button id="subtract" value="-" display="−" class="oper row-5 col-4" click={this.operInput} />
+        <Button id="one" value="1" display="1" class="num row-6 col-1" click={this.numInput} />
+        <Button id="two" value="2" display="2" class="num row-6 col-2" click={this.numInput} />
+        <Button id="three" value="3" display="3" class="num row-6 col-3" click={this.numInput} />
+        <Button id="add" value="+" display="+" class="oper row-6 col-4" click={this.operInput} />
+        <Button id="zero" value="0" display="0" class="num row-7 col-1-2" click={this.numInput} />
+        <Button id="decimal" value="." display="." class="num row-7 col-3" click={this.decInput} />
+        <Button id="equals" value="=" display="=" class="oper row-7 col-4" click={this.calculate} />
+      </div>
+    );
   }
 }
 
-clearInput() {
-  this.setState({
-    display: '0',
-    equation: ''
-  });
-}
+const Display = props => <div id="calc-display" className="row-1-2 col-1-4"><span id="eq">{props.equation}</span><span id="dis">{props.display}</span></div>;
 
-calculate() {
-  if(this.state.equation.includes('=')) {
-    let val = `${this.state.display} = ${this.state.display}`;
-    this.setState({
-      equation: val
-    });
-  } else if(this.state.equation != '' && this.state.equation.match(/[+\-*\/]/) != null && this.state.equation.match(/[+\-*\/]$/)) {
-    let result = Number.isInteger(eval(this.state.equation)) ? eval(this.state.equation) : parseFloat(eval(this.state.equation).toFixed(5));
-    let val = this.state.equation;
-    val += ` = ${result}`;
-    this.setState({
-      display: result,
-      equation: val
-    });
-  }
-}
+const Button = props => <button type="button" id={props.id} value={props.value} className={props.class} onClick={props.click}>{props.display}</button>;
 
-
-
-}
+export default App;
